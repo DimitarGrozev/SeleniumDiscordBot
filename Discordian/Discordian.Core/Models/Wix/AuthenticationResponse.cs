fbafd0ecba349d011b0481dc3de6e1a0ba50b850
DimitarGrozev <d.grozev99@gmail.com>
2022-02-05 21:54:09 +0200

    Initial login logic

---
+++ Discordian/Discordian.Core/Models/Wix/AuthenticationResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Discordian.Core.Models.Wix
+{
+	public class AuthenticationResponse
+	{
+		[JsonPropertyName("isSubscribed")]
+		public bool IsSubscribed { get; set; }
+	}
+}
--- Discordian/Discordian/Views/MainPage.xaml.cs
+++ Discordian/Discordian/Views/MainPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Discordian.Core.Helpers;
 using Discordian.Core.Models.Charts;
+using Discordian.Core.Models.Wix;
 using Discordian.Services;
 using Discordian.Utilities;
 using Discordian.ViewModels;
@@ -11,11 +14,14 @@ using Windows.UI.Core.Preview;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.Web.Http;
 
 namespace Discordian.Views
 {
     public sealed partial class MainPage : Page
     {
+        private readonly string AuthorizeURL = "https://dgrozev99.wixsite.com/my-site/_functions-dev/authorize";
+        private static bool isLoggedIn = false;
         public MainViewModel ViewModel { get; } = new MainViewModel();
 
         public MainPage()
@@ -27,6 +33,45 @@ namespace Discordian.Views
 
         private async void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
+            if (!isLoggedIn)
+            {
+                await this.LoginContentDialog.ShowAsync();
+            }
+            else
+            {
+                await this.LoadStatisticsDataAsync();
+            }
+        }
+
+        private void OnCloseRequest(object sender, SystemNavigationCloseRequestedPreviewEventArgs e)
+        {
+            DiscordianBotConsoleClient.StopAllBots();
+        }
+
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginSpinner.IsActive = true;
+
+            var isSubscribed = await this.CheckIfUserIsSubscribedAsync();
+
+            LoginSpinner.IsActive = false;
+
+            if (isSubscribed)
+            {
+                await this.LoadStatisticsDataAsync();
+
+                isLoggedIn = true;
+            }
+            else
+            {
+                this.AccountValidationErrorMessage.Text = "Please renew your subscription!";
+            }
+        }
+
+        private async Task LoadStatisticsDataAsync()
+        {
+            this.LoginContentDialog.Hide();
+
             if (App.Connection == null)
             {
                 await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync("Id");
@@ -77,9 +122,30 @@ namespace Discordian.Views
             ProgressSpinnerForMentionsPerBot.IsActive = false;
         }
 
-        private void OnCloseRequest(object sender, SystemNavigationCloseRequestedPreviewEventArgs e)
+        private async Task<bool> CheckIfUserIsSubscribedAsync()
         {
-            DiscordianBotConsoleClient.StopAllBots();
+            var email = this.EmailTextBox.Text;
+            var password = this.PasswordTextBox.Password;
+
+            try
+            {
+                var httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36");
+                var httpRequestMessage = new HttpRequestMessage();
+                var content = new HttpStringContent(await Json.StringifyAsync(new User { email = email, password = password }), Windows.Storage.Streams.UnicodeEncoding.Utf8, "application/json");
+                var url = new Uri(AuthorizeURL);
+
+                var responseMessage = await httpClient.PostAsync(url, content);
+                var responseBody = await responseMessage.Content.ReadAsStringAsync();
+                var data = await Json.ToObjectAsync<AuthenticationResponse>(responseBody);
+
+                return data.IsSubscribed;
+            }
+            catch (Exception)
+            {
+            }
+
+            return false;
         }
     }
 }
